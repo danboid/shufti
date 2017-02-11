bf87800a4f82afbf05efe0b00a01db31487ecc4e
Dan MacDonald <allcoms@gmail.com>
2017-02-11 22:28:22 +0000

    Add window title and resize QGraphicsView so as to be fit for use

--- shufti.py
+++ shufti.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+#
 # shufti - A WIP, PyQt5 persistent image viewer for Gahnooh slaash Leenoox.
+#
+# By Dan MacDonald
+#
+# 2017
 
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene, QGraphicsView
@@ -11,6 +16,8 @@ class Shufti(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.setWindowTitle("shufti")
+        self.resize(999, 999)
         
         
     def initUI(self):               
@@ -18,6 +25,7 @@ class Shufti(QMainWindow):
         self.scene = QGraphicsScene()
         self.scene.addPixmap(QPixmap("/home/dan/pic.jpg"))
         self.view = QGraphicsView(self.scene, self)
+        self.view.resize(999, 999)
         self.show()
         
         
